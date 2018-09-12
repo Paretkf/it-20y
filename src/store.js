00d5bb7a0d44159c30cbff8f4956dffbc2d61356
Paretkf <paretk7@gmail.com>
2018-09-12 18:21:24 +0700

    first and login fb

---
+++ src/store.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import firebase from 'firebase'
+const config = {
+  apiKey: 'AIzaSyB2LD6TMfj2bM12LQHxu8buuEovh7x8J80',
+  authDomain: 'it-20y.firebaseapp.com',
+  databaseURL: 'https://it-20y.firebaseio.com',
+  projectId: 'it-20y',
+  storageBucket: 'it-20y.appspot.com',
+  messagingSenderId: '68611767349'
+}
+firebase.initializeApp(config)
+const provider = new firebase.auth.FacebookAuthProvider()
+provider.addScope('public_profile')
+provider.setCustomParameters({
+  'display': 'popup'
+})
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    user: {}
+  },
+  mutations: {
+    SET_USER (state, data) {
+      state.user = data
+    },
+    LOG_OUT (state) {
+      state.user = {}
+    }
+  },
+  actions: {
+    login ({commit}) {
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        commit('SET_USER', result)
+      })
+    },
+    logout ({commit}) {
+      firebase.auth().signOut().then(() => {
+        commit('LOG_OUT')
+      })
+    },
+  }
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <button @click="login()"> Login </button>
+    <div v-if="user.user">
+       <img :src="user.user.photoURL" alt=""> <br>
+      {{user.user.displayName}} <br>
+      <button @click="logout()"> Logout </button>
+    </div>
+   
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    ...mapActions({
+      login: 'login',
+      logout: 'logout'
+    })
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user
+    })
+  }
+}
+</script>
